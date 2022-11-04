6efb57459550806e77035402768e7e041c4d4877
daniel <dan.doyin@gmail.com>
2022-11-04 04:22:14 -0700

    master

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+Random numberGen = new Random();
+
+
+int roll = 0;
+int attempts = 0;
+
+while (roll != 0)
+{
+    roll = numberGen.Next(1, 2);
+}
 
 
 
