9f36ec5c0e30191505ccf76ea84d5bb9960dffa9
daniel <dan.doyin@gmail.com>
2022-11-05 03:01:49 -0700

    created a ranking system with arrays

--- Program.cs
+++ Program.cs
@@ -1,24 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Random numberGen = new Random();
+// Random numberGen = new Random();
 
 
-int rollOne = 0;
-int rollTwo = 1;
-int attempts = 0;
-Console.WriteLine("press enter to roll a die");
+// int rollOne = 0;
+// int rollTwo = 1;
+// int attempts = 0;
+// Console.WriteLine("press enter to roll a die");
 
-while (rollOne != rollTwo )
-{Console.ReadKey();
+// while (rollOne != rollTwo )
+// {Console.ReadKey();
 
-    rollOne = numberGen.Next(1, 7);
-    rollTwo = numberGen.Next(1, 7);
-    Console.WriteLine("you rolled: " + rollOne);
-        Console.WriteLine("Roll 2: " + rollTwo + "\n");
-    attempts++;
+//     rollOne = numberGen.Next(1, 7);
+//     rollTwo = numberGen.Next(1, 7);
+//     Console.WriteLine("you rolled: " + rollOne);
+//         Console.WriteLine("Roll 2: " + rollTwo + "\n");
+//     attempts++;
+// }
+
+// Console.WriteLine("it took you " + attempts + " attempts to roll a six");
+
+
+String[]  shopping = {"bread", "leaves", "toys", "cream"};
+
+for (int i = 0; i < shopping.Length; i++)
+{
+    int rank = i + 1;
+    Console.WriteLine(rank + " " + shopping[i]);
 }
-
-Console.WriteLine("it took you " + attempts + " attempts to roll a six");
 
 Console.Read();
